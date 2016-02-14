94ac7ec9ee4f63a0a5c3b784d2a1c2b158449aad
HecMartinSol <hecmartinsol@gmail.com>
2016-02-14 20:27:55 +0100

    Refactorizado y eliminada una clase. No hay más cambios pero he hecho documentación xD

--- src/java/MainClass.java
+++ src/java/MainClass.java
@@ -1,29 +1,14 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
+import java.io.*;
 import jason.JasonException;
 import jason.architecture.AgArch;
 import jason.asSemantics.Agent;
 import jason.asSemantics.Circumstance;
 import jason.asSemantics.TransitionSystem;
-import jason.asSyntax.Literal;
-import jason.asSyntax.Term;
-import jason.bb.BeliefBase;
-import jason.environment.Environment;
-import jason.jeditplugin.NewEnvironmentGUI;
-import jason.mas2j.AgentParameters;
-import jason.mas2j.MAS2JProject;
+import jason.mas2j.*;
 import jason.mas2j.parser.ParseException;
 import jason.runtime.Settings;
-import jason.stdlib.foreach;
-import sun.management.resources.agent;
 
-public class TransformerClass {
+public class MainClass {
 	
 	public static void main(String[] args) throws ParseException, FileNotFoundException, JasonException {
 		System.out.println("EJECUTANDO A LA VEZ");
